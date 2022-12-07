9f22db9424176c9befb130eeb7009a09589c1693
GimenaRiveros <gimena1001riveros@gmail.com>
2022-12-07 14:46:24 -0300

    Actualizo la tabla de datos de usuario a mi dashboard. MGR

--- src/app/components/dashboard/users/users.component.ts
+++ src/app/components/dashboard/users/users.component.ts
@@ -3,9 +3,17 @@ import { User } from 'src/app/interfaces/users';
 
 
 const listUsers: User[] = [
+  {user: "hyalejandria", name: "Hypatia", surname: "deAlejandria", sex: "Female"},
+  {user: "leodavinci", name: "Leonardo", surname: "daVinci", sex: "Male"},
+  {user: "lfSoto", name: "Lucas", surname: "Soto", sex: "Male"},
+  {user: "macurie", name: "Marie", surname: "Curie", sex: "Female"},
+  {user: "jperalta", name: "John", surname: "Peralta", sex: "Male"},
+  {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
+  {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
+  {user: "ngarcia", name: "Noe", surname: "Garcia", sex: "Male"},
   {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
   {user: "MarceloOK", name: "Claudio", surname: "Imberlina", sex: "Male"},
- 
+  {user: "jsmith", name: "John", surname: "Smith", sex: "Male"},
 ]
 
 @Component({
