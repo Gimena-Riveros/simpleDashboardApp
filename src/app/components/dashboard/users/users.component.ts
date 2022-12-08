14984c66b5d41bd221c3dd65506d5489c1056a9a
GimenaRiveros <gimena1001riveros@gmail.com>
2022-12-08 10:08:50 -0300

    Implementamos filtro en la tabla de usuarios. MGR

--- src/app/components/dashboard/users/users.component.ts
+++ src/app/components/dashboard/users/users.component.ts
@@ -1,20 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/interfaces/users';
 
-
-const listUsers: User[] = [
-  {user: "hyalejandria", name: "Hypatia", surname: "deAlejandria", sex: "Female"},
-  {user: "leodavinci", name: "Leonardo", surname: "daVinci", sex: "Male"},
-  {user: "lfSoto", name: "Lucas", surname: "Soto", sex: "Male"},
-  {user: "macurie", name: "Marie", surname: "Curie", sex: "Female"},
-  {user: "jperalta", name: "John", surname: "Peralta", sex: "Male"},
-  {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
-  {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
-  {user: "ngarcia", name: "Noe", surname: "Garcia", sex: "Male"},
-  {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
-  {user: "alturing", name: "Allan", surname: "Turing", sex: "Male"},
-  {user: "jsmith", name: "John", surname: "Smith", sex: "Male"},
-]
 
 @Component({
   selector: 'app-users',
@@ -23,11 +10,28 @@ const listUsers: User[] = [
 })
 export class UsersComponent implements OnInit {
 
-  displayedColumns: string[] = ['user', 'name', 'surname', 'sex'];
-  dataSource = listUsers;
+  listUsers: User[] = [
+    {user: "hyalejandria", name: "Hypatia", surname: "deAlejandria", sex: "Female"},
+    {user: "leodavinci", name: "Leonardo", surname: "daVinci", sex: "Male"},
+    {user: "lfSoto", name: "Lucas", surname: "Soto", sex: "Male"},
+    {user: "macurie", name: "Marie", surname: "Curie", sex: "Female"},
+    {user: "jperalta", name: "John", surname: "Peralta", sex: "Male"},
+    {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
+    {user: "GimeOK", name: "Gimena", surname: "Riveros", sex: "Female"},
+    {user: "ngarcia", name: "Noe", surname: "Garcia", sex: "Male"},
+    {user: "alturing", name: "Allan", surname: "Turing", sex: "Male"},
+    {user: "jsmith", name: "John", surname: "Smith", sex: "Male"},
+  ];
+
+  displayedColumns: string[] = ['user', 'name', 'surname', 'sex', 'actions'];
+  dataSource = new MatTableDataSource(this.listUsers);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/components/shared-angular/shared-angular.module.ts
+++ src/app/components/shared-angular/shared-angular.module.ts
@@ -14,6 +14,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 /*SON TODOS LOS COMPONENTES USADOS POR LA APP EN UN SOLO .module.ts*/
 @NgModule({
@@ -30,7 +31,8 @@ import { MatTableModule } from '@angular/material/table';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
-    MatTableModule  
+    MatTableModule,
+    MatTooltipModule 
   ],
   exports: [
     MatSliderModule,
@@ -43,7 +45,8 @@ import { MatTableModule } from '@angular/material/table';
     MatToolbarModule,
     MatIconModule,
     HttpClientModule,
-    MatTableModule
+    MatTableModule,
+    MatTooltipModule
   ]
 })
 export class SharedAngularModule { }
